2e7a2d9edeb204267e93e847936c1c4269bfbe09
Rafal Lindemann <rl@stamina.pl>
2012-12-21 21:03:01 +0100

    fixes

---
+++ make.sh
@@ -0,0 +1,4 @@
+lessc public_html/css/konnekt.less --yui-compress -O0 > public_html/css/konnekt.css
+lessc public_html/bootstrap/less/bootstrap.less --yui-compress -O0 > public_html/bootstrap/css/bootstrap.css
+lessc public_html/bootstrap/less/responsive.less --yui-compress -O0 > public_html/bootstrap/css/bootstrap-responsive.css
+
